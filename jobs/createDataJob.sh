238a5c22fe22441ba1b3b381abfd3acfb72f614d
Lukas Geis <lukasgeis@gmx.de>
2022-10-26 23:48:55 +0200

    fix job typo

--- jobs/createDataJob.sh
+++ jobs/createDataJob.sh
@@ -13,10 +13,10 @@ python3 ../scripts/createData.py --number 30 --na_min 10 --na_max 99 --nb_min 10
 python3 ../scripts/createData.py --number 30 --na_min 10 --na_max 99 --nb_min 10 --nb_max 99 --vs_min 2 --vs_max 9 --output /scratch/memhierarchy/geis/bpr/data/1/1
 python3 ../scripts/createData.py --number 30 --na_min 10 --na_max 99 --nb_min 10 --nb_max 99 --vs_min 10 --vs_max 99 --output /scratch/memhierarchy/geis/bpr/data/1/2
 
-python3 ../scripts/createData.py --number 30 --na_min 100 --na_max 999 --nb_min 100 --nb_max 999 --vs_min 1 --vs_max 1 --output /scratch/memhierarchy/geis/bpr/data/1/0
-python3 ../scripts/createData.py --number 30 --na_min 100 --na_max 999 --nb_min 100 --nb_max 999 --vs_min 2 --vs_max 9 --output /scratch/memhierarchy/geis/bpr/data/1/1
-python3 ../scripts/createData.py --number 30 --na_min 100 --na_max 999 --nb_min 100 --nb_max 999 --vs_min 10 --vs_max 99 --output /scratch/memhierarchy/geis/bpr/data/1/2
+python3 ../scripts/createData.py --number 30 --na_min 100 --na_max 999 --nb_min 100 --nb_max 999 --vs_min 1 --vs_max 1 --output /scratch/memhierarchy/geis/bpr/data/2/0
+python3 ../scripts/createData.py --number 30 --na_min 100 --na_max 999 --nb_min 100 --nb_max 999 --vs_min 2 --vs_max 9 --output /scratch/memhierarchy/geis/bpr/data/2/1
+python3 ../scripts/createData.py --number 30 --na_min 100 --na_max 999 --nb_min 100 --nb_max 999 --vs_min 10 --vs_max 99 --output /scratch/memhierarchy/geis/bpr/data/2/2
 
-python3 ../scripts/createData.py --number 30 --na_min 1000 --na_max 9999 --nb_min 1000 --nb_max 9999 --vs_min 1 --vs_max 1 --output /scratch/memhierarchy/geis/bpr/data/1/0
-python3 ../scripts/createData.py --number 30 --na_min 1000 --na_max 9999 --nb_min 1000 --nb_max 9999 --vs_min 2 --vs_max 9 --output /scratch/memhierarchy/geis/bpr/data/1/1
-python3 ../scripts/createData.py --number 30 --na_min 1000 --na_max 9999 --nb_min 1000 --nb_max 9999 --vs_min 10 --vs_max 99 --output /scratch/memhierarchy/geis/bpr/data/1/2
+python3 ../scripts/createData.py --number 30 --na_min 1000 --na_max 9999 --nb_min 1000 --nb_max 9999 --vs_min 1 --vs_max 1 --output /scratch/memhierarchy/geis/bpr/data/3/0
+python3 ../scripts/createData.py --number 30 --na_min 1000 --na_max 9999 --nb_min 1000 --nb_max 9999 --vs_min 2 --vs_max 9 --output /scratch/memhierarchy/geis/bpr/data/3/1
+python3 ../scripts/createData.py --number 30 --na_min 1000 --na_max 9999 --nb_min 1000 --nb_max 9999 --vs_min 10 --vs_max 99 --output /scratch/memhierarchy/geis/bpr/data/3/2
