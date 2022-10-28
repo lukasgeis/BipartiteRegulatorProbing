b254adeb9447a4c4c092153824089074798f29b8
Lukas Geis <lukasgeis@gmx.de>
2022-10-28 17:08:11 +0200

    SUM variant testing 2

--- src/main.rs
+++ src/main.rs
@@ -41,6 +41,7 @@ fn main() -> std::io::Result<()> {
 
     let mut instance: Instance = bpr.create_instance();
     instance.optimal_solution(GoalType::MAX, opt.l);
+    instance.optimal_solution(GoalType::SUM, opt.l);
     instance.namp(GoalType::MAX, opt.k, opt.l);
     instance.namp(GoalType::SUM, opt.k, opt.l);
     println!("{:?}", instance.get_realizations());
