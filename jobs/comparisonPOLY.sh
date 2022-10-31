beb802915abd2f4240120d6abf45c40c982032f5
Lukas Geis <lukasgeis@gmx.de>
2022-10-31 13:43:47 +0100

    fixed spelling mistake in comparisonPOLY.sh

--- jobs/comparisonPOLY.sh
+++ jobs/comparisonPOLY.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=bprpoly
+#SBATCH --job-name=bprPOLY
 #SBATCH --partition=general1
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
@@ -11,47 +11,47 @@
 
 for file in /scratch/memhierarchy/geis/bpr/data/1/0/*
 do
-    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/poly/comparison10 --iterations 10 --algorithm poly --fraction 4
+    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/POLY/comparison10 --iterations 10 --algorithm POLY --fraction 4
 done
 
 for file in /scratch/memhierarchy/geis/bpr/data/1/1/*
 do
-    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/poly/comparison11 --iterations 10 --algorithm poly --fraction 4
+    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/POLY/comparison11 --iterations 10 --algorithm POLY --fraction 4
 done
 
 for file in /scratch/memhierarchy/geis/bpr/data/1/2/*
 do
-    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/poly/comparison12 --iterations 10 --algorithm poly --fraction 4
+    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/POLY/comparison12 --iterations 10 --algorithm POLY --fraction 4
 done
 
 
 for file in /scratch/memhierarchy/geis/bpr/data/2/0/*
 do
-    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/poly/comparison20 --iterations 10 --algorithm poly --fraction 4
+    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/POLY/comparison20 --iterations 10 --algorithm POLY --fraction 4
 done
 
 for file in /scratch/memhierarchy/geis/bpr/data/2/1/*
 do
-    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/poly/comparison21 --iterations 10 --algorithm poly --fraction 4
+    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/POLY/comparison21 --iterations 10 --algorithm POLY --fraction 4
 done
 
 for file in /scratch/memhierarchy/geis/bpr/data/2/2/*
 do
-    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/poly/comparison22 --iterations 10 --algorithm poly --fraction 4
+    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/POLY/comparison22 --iterations 10 --algorithm POLY --fraction 4
 done
 
 
 for file in /scratch/memhierarchy/geis/bpr/data/3/0/*
 do
-    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/poly/comparison30 --iterations 10 --algorithm poly --fraction 4
+    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/POLY/comparison30 --iterations 10 --algorithm POLY --fraction 4
 done
 
 for file in /scratch/memhierarchy/geis/bpr/data/3/1/*
 do
-    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/poly/comparison31 --iterations 10 --algorithm poly --fraction 4
+    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/POLY/comparison31 --iterations 10 --algorithm POLY --fraction 4
 done
 
 for file in /scratch/memhierarchy/geis/bpr/data/3/2/*
 do
-    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/poly/comparison32 --iterations 10 --algorithm poly --fraction 4
+    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/POLY/comparison32 --iterations 10 --algorithm POLY --fraction 4
 done
