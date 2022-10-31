717e14615c37b84d49a6d73e28471e426abd4f04
Lukas Geis <lukasgeis@gmx.de>
2022-10-31 20:20:48 +0100

    parallized jobs

--- jobs/bruteforceNAMP.sh
+++ jobs/bruteforceNAMP.sh
@@ -2,56 +2,72 @@
 #SBATCH --job-name=bprnampbf
 #SBATCH --partition=general1
 #SBATCH --nodes=1
-#SBATCH --ntasks=1
+#SBATCH --ntasks=30
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=1200
+#SBATCH --mem-per-cpu=10000
 #SBATCH --time=100:00:00
 #SBATCH --no-requeue
 #SBATCH --mail-type=FAIL
 
 for file in /scratch/memhierarchy/geis/bpr/data/1/0/*
 do
-    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/namp/bruteforce10 --input-time /scratch/memhierarchy/geis/bpr/logs/inputtimes --iterations 10 --algorithm NAMP --bruteforce
+    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/namp/bruteforce10 --input-time /scratch/memhierarchy/geis/bpr/logs/inputtimes --iterations 10 --algorithm NAMP --bruteforce &
 done
+
+wait
 
 for file in /scratch/memhierarchy/geis/bpr/data/1/1/*
 do
-    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/namp/bruteforce11 --input-time /scratch/memhierarchy/geis/bpr/logs/inputtimes --iterations 10 --algorithm NAMP --bruteforce
+    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/namp/bruteforce11 --input-time /scratch/memhierarchy/geis/bpr/logs/inputtimes --iterations 10 --algorithm NAMP --bruteforce & 
 done
+
+wait
 
 for file in /scratch/memhierarchy/geis/bpr/data/1/2/*
 do
-    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/namp/bruteforce12 --input-time /scratch/memhierarchy/geis/bpr/logs/inputtimes --iterations 10 --algorithm NAMP --bruteforce
+    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/namp/bruteforce12 --input-time /scratch/memhierarchy/geis/bpr/logs/inputtimes --iterations 10 --algorithm NAMP --bruteforce &
 done
+
+wait
 
 
 for file in /scratch/memhierarchy/geis/bpr/data/2/0/*
 do
-    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/namp/bruteforce20 --input-time /scratch/memhierarchy/geis/bpr/logs/inputtimes --iterations 10 --algorithm NAMP --bruteforce
+    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/namp/bruteforce20 --input-time /scratch/memhierarchy/geis/bpr/logs/inputtimes --iterations 10 --algorithm NAMP --bruteforce &
 done
+
+wait
 
 for file in /scratch/memhierarchy/geis/bpr/data/2/1/*
 do
-    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/namp/bruteforce21 --input-time /scratch/memhierarchy/geis/bpr/logs/inputtimes --iterations 10 --algorithm NAMP --bruteforce
+    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/namp/bruteforce21 --input-time /scratch/memhierarchy/geis/bpr/logs/inputtimes --iterations 10 --algorithm NAMP --bruteforce &
 done
+
+wait
 
 for file in /scratch/memhierarchy/geis/bpr/data/2/2/*
 do
-    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/namp/bruteforce22 --input-time /scratch/memhierarchy/geis/bpr/logs/inputtimes --iterations 10 --algorithm NAMP --bruteforce
+    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/namp/bruteforce22 --input-time /scratch/memhierarchy/geis/bpr/logs/inputtimes --iterations 10 --algorithm NAMP --bruteforce &
 done
+
+wait
 
 
 for file in /scratch/memhierarchy/geis/bpr/data/3/0/*
 do
-    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/namp/bruteforce30 --input-time /scratch/memhierarchy/geis/bpr/logs/inputtimes --iterations 10 --algorithm NAMP --bruteforce
+    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/namp/bruteforce30 --input-time /scratch/memhierarchy/geis/bpr/logs/inputtimes --iterations 10 --algorithm NAMP --bruteforce &
 done
+
+wait
 
 for file in /scratch/memhierarchy/geis/bpr/data/3/1/*
 do
-    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/namp/bruteforce31 --input-time /scratch/memhierarchy/geis/bpr/logs/inputtimes --iterations 10 --algorithm NAMP --bruteforce
+    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/namp/bruteforce31 --input-time /scratch/memhierarchy/geis/bpr/logs/inputtimes --iterations 10 --algorithm NAMP --bruteforce &
 done
+
+wait
 
 for file in /scratch/memhierarchy/geis/bpr/data/3/2/*
 do
-    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/namp/bruteforce32 --input-time /scratch/memhierarchy/geis/bpr/logs/inputtimes --iterations 10 --algorithm NAMP --bruteforce
+    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/namp/bruteforce32 --input-time /scratch/memhierarchy/geis/bpr/logs/inputtimes --iterations 10 --algorithm NAMP --bruteforce &
 done
--- jobs/comparisonPOLY.sh
+++ jobs/comparisonPOLY.sh
@@ -2,56 +2,72 @@
 #SBATCH --job-name=bprPOLY
 #SBATCH --partition=general1
 #SBATCH --nodes=1
-#SBATCH --ntasks=1
+#SBATCH --ntasks=30
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=1200
+#SBATCH --mem-per-cpu=10000
 #SBATCH --time=100:00:00
 #SBATCH --no-requeue
 #SBATCH --mail-type=FAIL
 
 for file in /scratch/memhierarchy/geis/bpr/data/1/0/*
 do
-    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/POLY/comparison10 --iterations 10 --algorithm POLY --fraction 4
+    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/poly/comparison10 --iterations 10 --algorithm POLY --fraction 4 &
 done
+
+wait
 
 for file in /scratch/memhierarchy/geis/bpr/data/1/1/*
 do
-    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/POLY/comparison11 --iterations 10 --algorithm POLY --fraction 4
+    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/poly/comparison11 --iterations 10 --algorithm POLY --fraction 4 &
 done
+
+wait
 
 for file in /scratch/memhierarchy/geis/bpr/data/1/2/*
 do
-    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/POLY/comparison12 --iterations 10 --algorithm POLY --fraction 4
+    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/poly/comparison12 --iterations 10 --algorithm POLY --fraction 4 &
 done
+
+wait
 
 
 for file in /scratch/memhierarchy/geis/bpr/data/2/0/*
 do
-    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/POLY/comparison20 --iterations 10 --algorithm POLY --fraction 4
+    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/poly/comparison20 --iterations 10 --algorithm POLY --fraction 4 &
 done
+
+wait
 
 for file in /scratch/memhierarchy/geis/bpr/data/2/1/*
 do
-    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/POLY/comparison21 --iterations 10 --algorithm POLY --fraction 4
+    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/poly/comparison21 --iterations 10 --algorithm POLY --fraction 4 &
 done
+
+wait
 
 for file in /scratch/memhierarchy/geis/bpr/data/2/2/*
 do
-    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/POLY/comparison22 --iterations 10 --algorithm POLY --fraction 4
+    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/poly/comparison22 --iterations 10 --algorithm POLY --fraction 4 &
 done
+
+wait
 
 
 for file in /scratch/memhierarchy/geis/bpr/data/3/0/*
 do
-    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/POLY/comparison30 --iterations 10 --algorithm POLY --fraction 4
+    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/poly/comparison30 --iterations 10 --algorithm POLY --fraction 4 &
 done
+
+wait
 
 for file in /scratch/memhierarchy/geis/bpr/data/3/1/*
 do
-    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/POLY/comparison31 --iterations 10 --algorithm POLY --fraction 4
+    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/poly/comparison31 --iterations 10 --algorithm POLY --fraction 4 &
 done
+
+wait
 
 for file in /scratch/memhierarchy/geis/bpr/data/3/2/*
 do
-    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/POLY/comparison32 --iterations 10 --algorithm POLY --fraction 4
+    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/poly/comparison32 --iterations 10 --algorithm POLY --fraction 4 &
 done
