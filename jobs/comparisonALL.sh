d838f892765f9e72f780c5c5f37f1c4de74c8fdb
Lukas Geis <lukasgeis@gmx.de>
2022-11-02 17:48:43 +0100

    ALL Algorithms Job

---
+++ jobs/comparisonALL.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=bprPOLY
+#SBATCH --partition=general1
+#SBATCH --nodes=1
+#SBATCH --ntasks=9
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=10000
+#SBATCH --time=100:00:00
+#SBATCH --no-requeue
+#SBATCH --mail-type=FAIL
+
+for file in /scratch/memhierarchy/geis/bpr/data/1/0/Random_na1*
+do
+    BASENAME="$(basename -- $file)"
+    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/poly/10/OUT_$BASENAME --iterations 10 --algorithm ALL --fraction 2 &
+done
+
+for file in /scratch/memhierarchy/geis/bpr/data/1/0/Random_na2*
+do
+    BASENAME="$(basename -- $file)"
+    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/poly/10/OUT_$BASENAME --iterations 10 --algorithm ALL --fraction 2 &
+done
