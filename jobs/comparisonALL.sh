ba95ae2850dd30dc7596bc1474a3b1786a89777f
Lukas Geis <lukasgeis@gmx.de>
2022-11-02 23:40:00 +0100

    small data all job

--- jobs/comparisonALL.sh
+++ jobs/comparisonALL.sh
@@ -2,21 +2,15 @@
 #SBATCH --job-name=bprall
 #SBATCH --partition=general1
 #SBATCH --nodes=1
-#SBATCH --ntasks=9
+#SBATCH --ntasks=10
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=10000
 #SBATCH --time=100:00:00
 #SBATCH --no-requeue
 #SBATCH --mail-type=FAIL
 
-for file in /scratch/memhierarchy/geis/bpr/data/1/0/Random_na1*
+for file in /scratch/memhierarchy/geis/bpr/data/0/*
 do
     BASENAME="$(basename -- $file)"
-    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/all/OUT_$BASENAME --iterations 10 --algorithm ALL --fraction 4 &
-done
-
-for file in /scratch/memhierarchy/geis/bpr/data/1/0/Random_na2*
-do
-    BASENAME="$(basename -- $file)"
-    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/all/OUT_$BASENAME --iterations 10 --algorithm ALL --fraction 4 &
+    target/release/bpr --input $file --log /scratch/memhierarchy/geis/bpr/logs/all/OUT_$BASENAME --iterations 10 --algorithm ALL --fraction 4
 done
--- jobs/createSmallData.sh
+++ jobs/createSmallData.sh
@@ -9,7 +9,7 @@
 #SBATCH --no-requeue
 #SBATCH --mail-type=FAIL
 
-python3 ../scripts/createData.py --number 30 --na_min 3 --na_max 10 --nb_min 2 --nb_max 10 --vs_min 1 --vs_max 1 --output /scratch/memhierarchy/geis/bpr/data/0
-python3 ../scripts/createData.py --number 30 --na_min 3 --na_max 10 --nb_min 2 --nb_max 10 --vs_min 2 --vs_max 2 --output /scratch/memhierarchy/geis/bpr/data/0
-python3 ../scripts/createData.py --number 30 --na_min 3 --na_max 10 --nb_min 2 --nb_max 10 --vs_min 3 --vs_max 3 --output /scratch/memhierarchy/geis/bpr/data/0
+python3 scripts/createData.py --number 30 --na_min 3 --na_max 10 --nb_min 2 --nb_max 10 --vs_min 1 --vs_max 1 --output /scratch/memhierarchy/geis/bpr/data/0
+python3 scripts/createData.py --number 30 --na_min 3 --na_max 10 --nb_min 2 --nb_max 10 --vs_min 2 --vs_max 2 --output /scratch/memhierarchy/geis/bpr/data/0
+python3 scripts/createData.py --number 30 --na_min 3 --na_max 10 --nb_min 2 --nb_max 10 --vs_min 3 --vs_max 3 --output /scratch/memhierarchy/geis/bpr/data/0
 
