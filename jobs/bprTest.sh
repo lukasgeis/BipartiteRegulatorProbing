909eeb2d02f9c5ae5c0254ba188b209fb5d21b9d
Lukas Geis <lukasgeis@gmx.de>
2022-11-12 19:52:13 +0100

    test job

---
+++ jobs/bprTest.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=bprFast
+#SBATCH --partition=test
+#SBATCH --nodes=1
+#SBATCH --ntasks=3
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=1200
+#SBATCH --time=120:00:00
+#SBATCH --no-requeue
+#SBATCH --mail-type=FAIL
+
+for NUM in 3 4 5 
+do
+    target/release/bpr --log /scratch/memhierarchy/geis/bpr/logs/test/OUT_$NUM --na $((2**$NUM)) --nb $((2**$NUM)) --vs $NUM --iterations 5 --instances 10 --algorithm FAST --parameters $((2**$NUM - 2)) &
+done
