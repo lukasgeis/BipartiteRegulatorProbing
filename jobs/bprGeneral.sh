9a201afeb7eca0794b43bd52ba1c06ddfb179dbd
Lukas Geis <lukasgeis@gmx.de>
2022-11-24 13:15:10 +0100

    experiments

---
+++ jobs/bprGeneral.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=bprFast
+#SBATCH --partition=general1
+#SBATCH --nodes=1
+#SBATCH --ntasks=60
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=2000
+#SBATCH --time=100:00:00
+#SBATCH --no-requeue
+#SBATCH --mail-type=FAIL
+
+OUTPUTDIR="/scratch/memhierarchy/geis/bpr/experiments/general"
+
+for NUM in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
+do
+    target/release/bpr --log "${OUTPUTDIR}/max/OUT_${NUM}" --na $((16 * $NUM)) --nb $((16 * $NUM)) --vs $((16 * $NUM)) --iterations 500 --instances 20 --goal MAX --algorithm FAST --parameters 4 &
+    target/release/bpr --log "${OUTPUTDIR}/sum/OUT_${NUM}" --na $((16 * $NUM)) --nb $((16 * $NUM)) --vs $((16 * $NUM)) --iterations 500 --instances 20 --goal SUM --algorithm FAST --parameters 4 &
+    target/release/bpr --log "${OUTPUTDIR}/cov/OUT_${NUM}" --na $((16 * $NUM)) --nb $((16 * $NUM)) --vs $((16 * $NUM)) --iterations 500 --instances 20 --goal COV --algorithm FAST --parameters 4 &
+done
+
+wait
