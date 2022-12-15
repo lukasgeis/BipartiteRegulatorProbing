736e5248f3e1e1d23eb599b07f1beefa8a959ca1
Lukas Geis <lukasgeis@gmx.de>
2022-12-15 12:15:05 +0100

    parameters compression script

---
+++ scripts/bprParametersCompression.py
@@ -0,0 +1,71 @@
+import os
+import pickle
+import argparse
+
+def main(inputDir: str, output: str):
+    X = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+    Y = [[[[0.0 for i in range(len(X))] for j in range(2)] for k in range(4)] for l in range(3)]
+    Z = [0 for i in range(len(X))]
+
+    for file in os.listdir(inputDir):
+        header = None
+        data = {}
+        for line in open(os.path.join(inputDir, file), "r").readlines():
+            content = [x.split(": ")[1] for x in line.split(" -- ")]
+            if header is None:
+                header = int(content[1])
+            if content[0] not in data:
+                data[content[0]] = {}
+            if content[11] not in data[content[0]]:
+                data[content[0]][content[11]] = []
+            data[content[0]][content[11]].append((content[5], int(content[6]), int(content[7]), int(content[10]))) 
+        if (header // 16) not in X:
+            break
+        idx = X.index(header // 16)
+
+        totWorst = [[[1.0, 1.0] for i in range(4)] for j in range(3)]
+        for bpr in data:
+            for instance in data[bpr]:
+                opt = {}  
+                for algo in data[bpr][instance]:
+                    if algo[0] == "OPT":
+                        opt[algo[2]] = algo[3]
+                    elif algo[0] == "AMP":
+                        k = (algo[1] // (header // 4)) - 1
+                        l = (algo[2] // (algo[1] // 4)) - 1
+                        ratio = algo[3] / opt[algo[2]]
+                        if ratio < totWorst[k][l][0]:
+                            totWorst[k][l][0] = ratio
+                    else:
+                        k = (algo[1] // (header // 4)) - 1
+                        l = (algo[2] // (algo[1] // 4)) - 1
+                        ratio = algo[3] / opt[algo[2]]
+                        if ratio < totWorst[k][l][1]:
+                            totWorst[k][l][1] = ratio
+        for k in range(3):
+            for l in range(4):
+                Y[k][l][0][idx] = totWorst[k][l][0]
+                Y[k][l][1][idx] = totWorst[k][l][1]
+        Z[idx] = len(data)
+    
+    fullData = {
+        "x": X,
+        "y": Y,
+        "z": Z,
+    }
+    
+    with open(output, "wb") as outfile:
+        pickle.dump(fullData, outfile)
+
+
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description = "Compress COV data")
+    parser.add_argument("--input", metavar = "", help = "Input Directory", required = True)
+    parser.add_argument("--output", metavar = "", help = "Output File", required = True)
+
+    args = parser.parse_args()
+
+    main(args.input, args.output)
