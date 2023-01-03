363033ba6a2b6787301051b1322bcf5f290f014d
Lukas Geis <lukasgeis@gmx.de>
2023-01-03 15:02:58 +0100

    changed instances from 20 to 10

--- jobs/bprGeneral.sh
+++ jobs/bprGeneral.sh
@@ -13,7 +13,7 @@ OUTPUTDIR="/scratch/memhierarchy/geis/bpr/experiments/general"
 
 for NUM in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
 do
-    target/release/bpr --log "${OUTPUTDIR}/max/OUT_${NUM}" --na $((16 * $NUM)) --nb $((16 * $NUM)) --vs $((16 * $NUM)) --iterations 500 --instances 20 --goal MAX --algorithm ALL --parameters 4 &
-    target/release/bpr --log "${OUTPUTDIR}/sum/OUT_${NUM}" --na $((16 * $NUM)) --nb $((16 * $NUM)) --vs $((16 * $NUM)) --iterations 500 --instances 20 --goal SUM --algorithm ALL --parameters 4 &
-    target/release/bpr --log "${OUTPUTDIR}/cov/OUT_${NUM}" --na $((16 * $NUM)) --nb $((16 * $NUM)) --vs $((16 * $NUM)) --iterations 500 --instances 20 --goal COV --algorithm ALL --parameters 4 &
+    target/release/bpr --log "${OUTPUTDIR}/max/OUT_${NUM}" --na $((16 * $NUM)) --nb $((16 * $NUM)) --vs $((16 * $NUM)) --iterations 500 --instances 10 --goal MAX --algorithm ALL --parameters 4 &
+    target/release/bpr --log "${OUTPUTDIR}/sum/OUT_${NUM}" --na $((16 * $NUM)) --nb $((16 * $NUM)) --vs $((16 * $NUM)) --iterations 500 --instances 10 --goal SUM --algorithm ALL --parameters 4 &
+    target/release/bpr --log "${OUTPUTDIR}/cov/OUT_${NUM}" --na $((16 * $NUM)) --nb $((16 * $NUM)) --vs $((16 * $NUM)) --iterations 500 --instances 10 --goal COV --algorithm ALL --parameters 4 &
 done
