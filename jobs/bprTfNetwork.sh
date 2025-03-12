50b9b39153ced776d6b6ff7c5e954a4558f169c8
lukasgeis <lukasgeis@gmx.de>
2025-03-12 16:55:47 +0100

    more iterations in tf-networks

--- jobs/bprTfNetwork.sh
+++ jobs/bprTfNetwork.sh
@@ -10,13 +10,15 @@ MEDIATED="tf_networks_data/mediated.txt"
 MEDIATED_SHUFFLED="tf_networks_data/mediated_shuffled.txt"
 RANDOM_GENES="tf_networks_data/random_genes.txt"
 
+ITERATIONS=500
+
 for KVAL in 30 40 50
 do
-    $BINARY --file $LEUKEMIA -k $KVAL -i 10 >> "${OUTPUTDIR}/leukemia.json" &
-    $BINARY --file $LEUKEMIA_SHUFFLED -k $KVAL -i 10 >> "${OUTPUTDIR}/leukemia_shuffled.json" &
-    $BINARY --file $MEDIATED -k $KVAL -i 10 >> "${OUTPUTDIR}/mediated.json" &
-    $BINARY --file $MEDIATED_SHUFFLED -k $KVAL -i 10 >> "${OUTPUTDIR}/mediated_shuffled.json" & 
-    $BINARY --file $RANDOM_GENES -k $KVAL -i 10 >> "${OUTPUTDIR}/random_genes.json" &
+    $BINARY --file $LEUKEMIA -k $KVAL -i $ITERATIONS >> "${OUTPUTDIR}/leukemia.json" &
+    $BINARY --file $LEUKEMIA_SHUFFLED -k $KVAL -i $ITERATIONS >> "${OUTPUTDIR}/leukemia_shuffled.json" &
+    $BINARY --file $MEDIATED -k $KVAL -i $ITERATIONS >> "${OUTPUTDIR}/mediated.json" &
+    $BINARY --file $MEDIATED_SHUFFLED -k $KVAL -i $ITERATIONS >> "${OUTPUTDIR}/mediated_shuffled.json" & 
+    $BINARY --file $RANDOM_GENES -k $KVAL -i $ITERATIONS >> "${OUTPUTDIR}/random_genes.json" &
 
     wait
     echo "Round ${KVAL} done"
